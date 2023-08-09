f283f33fca3195b1dc29bde6a629e2190a140084
John Francis <john9francis@gmail.com>
2023-08-09 00:00:24 -0600

    using webpack, got the code to run in a browser!

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # simple-excaliburjs
 A very simple game in excalibur JS
 
+# How to contribute:
+If you make any changes to the code, you must run the following commands:
+```ps
+tsc
+```
+This compiles the typescript from "src" into javascript in "build". Then run,
+```ps
+npx webpack --config webpack.config.js
+```
+This turns the "build" javascript into some javascript that the browser can read and puts it in the "main.js" file in "dist/". 
+
 # Useful Websites
 - [Getting started with typescript](https://www.w3schools.com/typescript/typescript_getstarted.php)
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import { Engine, DisplayMode, Input } from 'excalibur';
+import * as ex from 'excalibur';
 
-const game = new Engine({
+const game = new ex.Engine({
   width: 800, // the width of the canvas
   height: 600, // the height of the canvas
   canvasElementId: '', // the DOM canvas element ID, if you are providing your own
-  displayMode: DisplayMode.FitScreen, // the display mode
-  pointerScope: Input.PointerScope.Document, // the scope of capturing pointer (mouse/touch) events
+  displayMode: ex.DisplayMode.FitScreen, // the display mode
+  pointerScope: ex.Input.PointerScope.Document, // the scope of capturing pointer (mouse/touch) events
 })
 // call game.start, which is a Promise
 game.start().then(function () {
