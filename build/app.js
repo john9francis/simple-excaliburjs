f64a386a1f227cd4bb95cc0844399fd002f5f6fc
John Francis <john9francis@gmail.com>
2023-08-08 23:47:58 -0600

    about to try webpack to get my code in a format that the browser can understand. May need to restore a couple of times.

--- build/app.js
+++ build/app.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("Hello world!");
+Object.defineProperty(exports, "__esModule", { value: true });
+const excalibur_1 = require("excalibur");
+const game = new excalibur_1.Engine({
+    width: 800,
+    height: 600,
+    canvasElementId: '',
+    displayMode: excalibur_1.DisplayMode.FitScreen,
+    pointerScope: excalibur_1.Input.PointerScope.Document, // the scope of capturing pointer (mouse/touch) events
+});
+// call game.start, which is a Promise
+game.start().then(function () {
+    // ready, set, go!
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!")
+import { Engine, DisplayMode, Input } from 'excalibur';
+
+const game = new Engine({
+  width: 800, // the width of the canvas
+  height: 600, // the height of the canvas
+  canvasElementId: '', // the DOM canvas element ID, if you are providing your own
+  displayMode: DisplayMode.FitScreen, // the display mode
+  pointerScope: Input.PointerScope.Document, // the scope of capturing pointer (mouse/touch) events
+})
+// call game.start, which is a Promise
+game.start().then(function () {
+  // ready, set, go!
+})
